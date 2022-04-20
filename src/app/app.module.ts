dd7ecebde7da1f807be7f823cfaedc003b307901
prokopovich_e@lwo.by <prokopovich_e@lwo.by>
2022-04-20 11:29:10 +0300

    commit update package.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ArtsLibModule } from 'arts-lib';
+import { ArtsLibModule } from 'button-angular-lib';
 
 @NgModule({
   declarations: [
